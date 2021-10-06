44d3bb0d142868fdad3d612c58b719b38d620fd9
mjms0214 <80668325+mjms0214@users.noreply.github.com>
2021-10-06 14:47:06 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
-const numbers = {
-  _a: 1,
-  _b: 2,
-  sum: 3,
-  calculate() {
-    console.log('calculate');
-    this.sum = this._a + this._b;
-  },
-  get a() {
-    return this._a;
-  },
-  get b() {
-    return this._b;
-  },
-  set a(value) {
-    console.log('a가 바뀝니다.');
-    this._a = value;
-    this.calculate();
-  },
-  set b(value) {
-    console.log('b가 바뀝니다.');
-    this._b = value;
-    this.calculate();
+function sumOf(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
   }
-};
+  return sum;
+}
 
-console.log(numbers.sum);
-numbers.a = 5;
-numbers.b = 7;
-numbers.a = 9;
-console.log(numbers.sum);
-console.log(numbers.sum);
-console.log(numbers.sum);
+const result = sumOf([1, 2, 3, 4, 5]);
+console.log(result);
