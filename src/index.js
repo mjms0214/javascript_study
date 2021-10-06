14623944da55de8a29dfde58d64dbeff63736323
mjms0214 <80668325+mjms0214@users.noreply.github.com>
2021-10-06 14:43:56 +0900

    get/ set 함수

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 const numbers = {
-  a: 1,
-  b: 2,
-  get sum() {
-    console.log('sum함수가 실행됩니다!');
-    return this.a + this.b;
+  _a: 1,
+  _b: 2,
+  sum: 3,
+  calculate() {
+    console.log('calculate');
+    this.sum = this._a + this._b;
+  },
+  get a() {
+    return this._a;
+  },
+  get b() {
+    return this._b;
+  },
+  set a(value) {
+    console.log('a가 바뀝니다.');
+    this._a = value;
+    this.calculate();
+  },
+  set b(value) {
+    console.log('b가 바뀝니다.');
+    this._b = value;
+    this.calculate();
   }
 };
 
 console.log(numbers.sum);
-numbers.b = 5;
+numbers.a = 5;
+numbers.b = 7;
+numbers.a = 9;
+console.log(numbers.sum);
+console.log(numbers.sum);
 console.log(numbers.sum);
